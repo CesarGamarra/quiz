eb65d360908114c1b5910e3a2541f6f48b1a3c70
Cesar Gamarra <cesar_gc89@hotmail.com>
2015-06-21 20:41:17 +0200

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -33,9 +33,11 @@ sequelize.sync().then(function(){
 	//success(..) ejecuta el manejador una vez creada la tabla
 	Quiz.count().then(function(count){
 		if(count===0){
-			Quiz.create({pregunta:'Capital de Italia',
+			Quiz.bulkCreate([{pregunta:'Capital de Italia',
 				    	respuesta:'Roma'
-			}).then(function(){console.log('Base de datos inicializada')});
+			},{pregunta:'Capital de Portugal',
+				    	respuesta:'Lisboa'
+			}]).then(function(){console.log('Base de datos inicializada')});
 		}
 	});
 });
