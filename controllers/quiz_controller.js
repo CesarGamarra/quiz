eaff4fdbed663d4af49efa50395103e135a7733b
Cesar Gamarra <cesar_gc89@hotmail.com>
2015-06-23 20:44:36 +0200

    Validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -24,7 +24,7 @@ exports.index=function(req,res,next){
 	}
 	models.Quiz.findAll({where:["upper(pregunta) like ?", search],order:'pregunta ASC'}).then(
 		function(quizes){
-			res.render('quizes/index.ejs',{quizes:quizes});
+			res.render('quizes/index.ejs',{quizes:quizes,errors:[]});
 		}
 	).catch(function(error){
 		next(error);
@@ -32,7 +32,7 @@ exports.index=function(req,res,next){
 };
 
 exports.show=function(req,res){
-	res.render('quizes/show',{quiz:req.quiz});
+	res.render('quizes/show',{quiz:req.quiz,errors:[]});
 };
 
 exports.answer=function(req,res){
@@ -40,7 +40,7 @@ exports.answer=function(req,res){
 	if(req.query.respuesta===req.quiz.respuesta){
 		resultado='Correcto';
 	}
-	res.render('quizes/answer',{quiz:req.quiz,respuesta:resultado});
+	res.render('quizes/answer',{quiz:req.quiz,respuesta:resultado,errors:[]});
 };
 
 exports.new=function(req,res){
@@ -50,17 +50,23 @@ exports.new=function(req,res){
 			respuesta:"Respuesta"
 		}
 	);
-	res.render('quizes/new',{quiz:quiz});
+	res.render('quizes/new',{quiz:quiz,errors:[]});
 };
 exports.create=function(req,res){
 	var quiz=models.Quiz.build(req.body.quiz);
 
 	//Guarda en DB los campos pregunta y respuesta de quiz
-	quiz.save({fields:["pregunta","respuesta"]}).then(function(){
-		res.redirect('/quizes');
+	quiz.validate().then(function(err){
+		if(err){
+			res.render('quizes/new',{quiz:quiz, errors:err.errors});
+		}else{
+			quiz.save({fields:["pregunta","respuesta"]}).then(function(){
+				res.redirect('/quizes');
+			});
+		}
 	});
-}
+};
 
 exports.author=function(req,res){
 	res.render('author');
-}
+};
