1fffba35fdf2e8305ac9f9e8ea6cb3eea0d05dfd
Cesar Gamarra <cesar_gc89@hotmail.com>
2015-07-01 20:02:36 +0200

    Entrega Modulo 8

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -47,7 +47,8 @@ exports.new=function(req,res){
 	var quiz=models.Quiz.build(
 		{
 			pregunta:"Pregunta",
-			respuesta:"Respuesta"
+			respuesta:"Respuesta",
+			tema:"Tema"
 		}
 	);
 	res.render('quizes/new',{quiz:quiz,errors:[]});
@@ -60,7 +61,7 @@ exports.create=function(req,res){
 		if(err){
 			res.render('quizes/new',{quiz:quiz, errors:err.errors});
 		}else{
-			quiz.save({fields:["pregunta","respuesta"]}).then(function(){
+			quiz.save({fields:["pregunta","respuesta","tema"]}).then(function(){
 				res.redirect('/quizes');
 			});
 		}
@@ -76,12 +77,13 @@ exports.edit=function(req,res){
 exports.update=function(req,res){
 	req.quiz.pregunta=req.body.quiz.pregunta;
 	req.quiz.respuesta=req.body.quiz.respuesta;
+	req.quiz.tema=req.body.quiz.tema;
 
 	req.quiz.validate().then(function(err){
 		if(err){
 			res.render('quizes/edit',{quiz:req.quiz, errors:err.errors});
 		}else{
-			req.quiz.save({fields:["pregunta","respuesta"]})
+			req.quiz.save({fields:["pregunta","respuesta","tema"]})
 			.then(function(){
 				res.redirect('/quizes');
 			});
--- models/models.js
+++ models/models.js
@@ -34,9 +34,11 @@ sequelize.sync().then(function(){
 	Quiz.count().then(function(count){
 		if(count===0){
 			Quiz.bulkCreate([{pregunta:'Capital de Italia',
-				    	respuesta:'Roma'
+				    	respuesta:'Roma',
+				    	tema:'otro'
 			},{pregunta:'Capital de Portugal',
-				    	respuesta:'Lisboa'
+				    	respuesta:'Lisboa',
+				    	tema:'otro'
 			}]).then(function(){console.log('Base de datos inicializada')});
 		}
 	});
